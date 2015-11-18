88edf1b51a7dcaeb4a650efaf42e25557ee94ff7
Markus Mayer <markus.mayer@wundercart.de>
2015-11-18 17:46:16 +0100

    build.sh now more verbose

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 source ./update-dockerfile.sh
 
 echo "Using Debian package mono-devel=$DEBIAN_MONO_VERSION of Mono $MONO_VERSION"
 
 echo "Building Docker image ..."
 docker build -t sunside/mono:$MONO_VERSION .
+
+echo "Built image sunside/mono:$MONO_VERSION"
+
 #docker tag -f sunside/mono:$MONO_VERSION sunside/mono:latest
+#echo "Tagged image sunside/mono:latest"
